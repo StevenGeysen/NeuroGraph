295ccb39461ddc6e1ba873a4c1eea8368d4718a1
Steven <steven_geysen@hotmail.com>
2024-03-15 17:12:23 +0100

    Fix presentation

--- src/Nucleus.py
+++ src/Nucleus.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""     NeuroGraph: Nucleus class -- Version 1
-Last edit:  2024/01/28
+"""     NeuroGraph: Nucleus class -- Version 1.1
+Last edit:  2024/03/15
 Author:     Geysen, Steven (SG)
-Notes:      - Nucleus class to build NeuroGraph.
+Notes:      - Nucleus class to build NeuroGraph
             - Release notes:
-                * Initial commit
+                * Fix presentation
 To do:      - Information in nodes
             - Filters
             - Plotting
@@ -16,14 +16,12 @@ Sources:    Python 3 Object-Oriented Programming (Phillips, 2018)
 
 
 
-#%% ~~ Imports and directories ~~ %%#
+#%% ~~ Imports ~~ %%#
 
 
-from pathlib import Path
+import abc
 
-
-# Directories
-SPINE = Path.cwd()
+from collections.abc import Container
 
 
 
@@ -31,7 +29,7 @@ SPINE = Path.cwd()
 ###########################
 
 
-class Nucleus(object):
+class Nucleus():
     """Nucleus node, containing all information for filtering and plotting."""
 
     _nucleus_keys = [
@@ -40,17 +38,40 @@ class Nucleus(object):
         ]
 
     def __init__(self, nucleus_dict=None):
+        # Create nucleus data dictionary
         if nucleus_dict == None:
-            nucleus_dict = {keyi: None for keyi in Nucleus._nucleus_keys}
+            nucleus_dict = {keyi: set() for keyi in Nucleus._nucleus_keys}
+        ## Add missing keys
+        missing_dict = {
+            keyi: set() for keyi in Nucleus._nucleus_keys - nucleus_dict.keys()
+            }
+        if not len(missing_dict.keys()) == 0:
+            nucleus_dict = nucleus_dict | missing_dict
         self._nucleus_dict = nucleus_dict
+    
     
     def connections(self, _nucleus_dict):
         return [
             nami['OfficialName'] for nami in _nucleus_dict['Connections']
             ]
     
+    
     def add_to_subregion(self, subregion):
         self._nucleus_dict['Subregion'] = subregion
+    
+    
+    def add_connection(self, brain_part):
+        self._nucleus_dict['Connections'].add(brain_part)
+        
+        #TODO: Update 'Connections' of both brain parts
+    
+    
+    def __repr__(self):
+        return f'Nucleus ({self._nucleus_dict["OfficialName"]})'
+    
+    
+    def __str__(self):
+        return self._nucleus_dict['OfficialName']
 
 
 
